2f2f8e92071d72d0689e4bf1d7cffc3d6f4e136d
bradkeys <bradkeys@gmail.com>
2023-02-04 00:25:45 -0500

    Firebase database experiment

--- Assets/Firebase/Sample/Database/UIHandler.cs
+++ Assets/Firebase/Sample/Database/UIHandler.cs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System.Linq;
+
 namespace Firebase.Sample.Database {
   using Firebase;
   using Firebase.Database;
@@ -26,20 +28,19 @@ namespace Firebase.Sample.Database {
   // startup.
   public class UIHandler : MonoBehaviour {
 
-    ArrayList leaderBoard = new ArrayList();
+    ArrayList rooms = new ArrayList();
+    private string roomCode = "";
+    private int score = 100;
+    private const int MaxScores = 5;
+    
     Vector2 scrollPosition = Vector2.zero;
     private Vector2 controlsScrollViewVector = Vector2.zero;
-
     public GUISkin fb_GUISkin;
-
-    private const int MaxScores = 5;
-    private string logText = "";
-    private string email = "";
-    private int score = 100;
     private Vector2 scrollViewVector = Vector2.zero;
     protected bool UIEnabled = true;
-
+    private string logText = "";
     const int kMaxLogSize = 16382;
+    
     DependencyStatus dependencyStatus = DependencyStatus.UnavailableOther;
     protected bool isFirebaseInitialized = false;
 
@@ -47,8 +48,7 @@ namespace Firebase.Sample.Database {
     // the required dependencies to use Firebase, and if not,
     // add them if possible.
     protected virtual void Start() {
-      leaderBoard.Clear();
-      leaderBoard.Add("Firebase Top " + MaxScores.ToString() + " Scores");
+      rooms.Clear();
 
       FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
         dependencyStatus = task.Result;
@@ -64,45 +64,35 @@ namespace Firebase.Sample.Database {
     // Initialize the Firebase database:
     protected virtual void InitializeFirebase() {
       FirebaseApp app = FirebaseApp.DefaultInstance;
+      FirebaseDatabase.DefaultInstance.GoOnline();
       StartListener();
       isFirebaseInitialized = true;
     }
 
     protected void StartListener() {
       FirebaseDatabase.DefaultInstance
-        .GetReference("Leaders").OrderByChild("score")
+        .GetReference("Rooms/ABCD/history")//.OrderByChild("created")
         .ValueChanged += (object sender2, ValueChangedEventArgs e2) => {
           if (e2.DatabaseError != null) {
             Debug.LogError(e2.DatabaseError.Message);
             return;
           }
-          Debug.Log("Received values for Leaders.");
-          string title = leaderBoard[0].ToString();
-          leaderBoard.Clear();
-          leaderBoard.Add(title);
+          
+          Debug.Log("Received values for rooms.");
+          
+          rooms.Clear();
+          
           if (e2.Snapshot != null && e2.Snapshot.ChildrenCount > 0) {
-            foreach (var childSnapshot in e2.Snapshot.Children) {
-              if (childSnapshot.Child("score") == null
-                  || childSnapshot.Child("score").Value == null) {
-                Debug.LogError("Bad data in sample.");
-                break;
-              } else {
-                Debug.Log("Leaders entry : " +
-                childSnapshot.Child("email").Value.ToString() + " - " +
-                childSnapshot.Child("score").Value.ToString());
-                leaderBoard.Insert(1, childSnapshot.Child("score").Value.ToString()
-                + "  " + childSnapshot.Child("email").Value.ToString());
-              }
+            foreach (var childSnapshot in e2.Snapshot.Children)
+            {
+              var code = childSnapshot.Child("playerId").Value.ToString();
+              // var created = childSnapshot.Child("created").Value.ToString();
+              // var label = code + " :: " + created;
+              // Debug.Log("room entry: " + label);
+              rooms.Insert(0, code);
             }
           }
-        };
-    }
-
-    // Exit if escape (or back, on mobile) is pressed.
-    protected virtual void Update() {
-      if (Input.GetKeyDown(KeyCode.Escape)) {
-        Application.Quit();
-      }
+      };
     }
 
     // Output text to the debug log text field, as well as the console.
@@ -122,52 +112,70 @@ namespace Firebase.Sample.Database {
     // and returns a TransactionResult which is either TransactionResult.Success(data) with
     // modified data or TransactionResult.Abort() which stops the transaction with no changes.
     TransactionResult AddScoreTransaction(MutableData mutableData) {
-      List<object> leaders = mutableData.Value as List<object>;
+      Debug.Log(mutableData.Value.ToString());
+      var rooms = mutableData.Value as List<object>;
+      Debug.Log("mutable children: " + mutableData.ChildrenCount);
 
-      if (leaders == null) {
-        leaders = new List<object>();
-      } else if (mutableData.ChildrenCount >= MaxScores) {
-        // If the current list of scores is greater or equal to our maximum allowed number,
-        // we see if the new score should be added and remove the lowest existing score.
-        long minScore = long.MaxValue;
-        object minVal = null;
-        foreach (var child in leaders) {
-          if (!(child is Dictionary<string, object>))
-            continue;
-          long childScore = (long)((Dictionary<string, object>)child)["score"];
-          if (childScore < minScore) {
-            minScore = childScore;
-            minVal = child;
-          }
-        }
-        // If the new score is lower than the current minimum, we abort.
-        if (minScore > score) {
-          return TransactionResult.Abort();
-        }
-        // Otherwise, we remove the current lowest to be replaced with the new score.
-        leaders.Remove(minVal);
+      if (rooms == null)
+      {
+        Debug.Log("rooms is null :(");
+        rooms = new List<object>();
       }
 
+      // } else if (mutableData.ChildrenCount >= MaxScores) {
+      //   // If the current list of scores is greater or equal to our maximum allowed number,
+      //   // we see if the new score should be added and remove the lowest existing score.
+      //   long minScore = long.MaxValue;
+      //   object minVal = null;
+      //   foreach (var child in rooms) {
+      //     if (!(child is Dictionary<string, object>))
+      //       continue;
+      //     long childScore = (long)((Dictionary<string, object>)child)["score"];
+      //     if (childScore < minScore) {
+      //       minScore = childScore;
+      //       minVal = child;
+      //     }
+      //   }
+      //   // If the new score is lower than the current minimum, we abort.
+      //   if (minScore > score) {
+      //     return TransactionResult.Abort();
+      //   }
+      //   // Otherwise, we remove the current lowest to be replaced with the new score.
+      //   rooms.Remove(minVal);
+      // }
+
+      var sampleEntry = new Dictionary<string, object>
+      {
+        ["playerId"] = roomCode,
+        ["cards"] = new List<string>(){"card_a", "card_b"}.ToArray(),
+      };
+      
       // Now we add the new score as a new entry that contains the email address and score.
-      Dictionary<string, object> newScoreMap = new Dictionary<string, object>();
-      newScoreMap["score"] = score;
-      newScoreMap["email"] = email;
-      leaders.Add(newScoreMap);
+      // Dictionary<string, object> newRoomMap = new Dictionary<string, object>
+      // {
+      //   ["created"] = DateTime.Now.ToString(),
+      //   // ["code"] = roomCode,
+      //   ["history"] = new List<Dictionary<string, object>>
+      //   {
+      //     sampleEntry
+      //   }
+      // };
+      // var roomList = history.ToList<Dictionary<string, object>>();
+      rooms.Add(sampleEntry);
 
       // You must set the Value to indicate data at that location has changed.
-      mutableData.Value = leaders;
+      mutableData.Value = rooms;
       return TransactionResult.Success(mutableData);
     }
 
     public void AddScore() {
-      if (score == 0 || string.IsNullOrEmpty(email)) {
-        DebugLog("invalid score or email.");
+      if (string.IsNullOrEmpty(roomCode)) {
+        DebugLog("invalid room code");
         return;
       }
-      DebugLog(String.Format("Attempting to add score {0} {1}",
-        email, score.ToString()));
+      DebugLog(String.Format("Attempting to add room code {0}", roomCode));
 
-      DatabaseReference reference = FirebaseDatabase.DefaultInstance.GetReference("Leaders");
+      DatabaseReference reference = FirebaseDatabase.DefaultInstance.GetReference("Rooms/ABCD/history");
 
       DebugLog("Running Transaction...");
       // Use a transaction to ensure that we do not encounter issues with
@@ -196,30 +204,30 @@ namespace Firebase.Sample.Database {
             GUILayout.BeginScrollView(controlsScrollViewVector);
         GUILayout.BeginVertical();
         GUILayout.BeginHorizontal();
-        GUILayout.Label("Email:", GUILayout.Width(Screen.width * 0.20f));
-        email = GUILayout.TextField(email);
+        GUILayout.Label("Room Code:", GUILayout.Width(Screen.width * 0.20f));
+        roomCode = GUILayout.TextField(roomCode);
         GUILayout.EndHorizontal();
 
         GUILayout.Space(20);
 
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Score:", GUILayout.Width(Screen.width * 0.20f));
-        int.TryParse(GUILayout.TextField(score.ToString()), out score);
-        GUILayout.EndHorizontal();
+        // GUILayout.BeginHorizontal();
+        // GUILayout.Label("Score:", GUILayout.Width(Screen.width * 0.20f));
+        // int.TryParse(GUILayout.TextField(score.ToString()), out score);
+        // GUILayout.EndHorizontal();
 
         GUILayout.Space(20);
 
-        if (!String.IsNullOrEmpty(email) && GUILayout.Button("Enter Score")) {
+        if (!String.IsNullOrEmpty(roomCode) && GUILayout.Button("Create Room")) {
           AddScore();
         }
 
         GUILayout.Space(20);
 
-        if (GUILayout.Button("Go Offline")) {
-          FirebaseDatabase.DefaultInstance.GoOffline();
-        }
-
-        GUILayout.Space(20);
+        // if (GUILayout.Button("Go Offline")) {
+        //   FirebaseDatabase.DefaultInstance.GoOffline();
+        // }
+        //
+        // GUILayout.Space(20);
 
         if (GUILayout.Button("Go Online")) {
           FirebaseDatabase.DefaultInstance.GoOnline();
@@ -235,7 +243,7 @@ namespace Firebase.Sample.Database {
       scrollPosition = GUILayout.BeginScrollView(scrollPosition, false, true);
       GUILayout.BeginVertical(GUI.skin.box);
 
-      foreach (string item in leaderBoard) {
+      foreach (string item in rooms) {
         GUILayout.Label(item, GUI.skin.box, GUILayout.ExpandWidth(true));
       }
 
